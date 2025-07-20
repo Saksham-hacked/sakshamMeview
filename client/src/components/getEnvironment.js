c6874adb05378e2b2705355cbb93547667b0966a
saksham kaushish <sakshamkaushishcodes@gmail.com>
2025-07-20 06:40:19 +0530

    fix: Update production URL in getEnvironment function for correct environment detection

--- client/src/components/getEnvironment.js
+++ client/src/components/getEnvironment.js
@@ -4,14 +4,14 @@
 function getEnvironment() {
   const currentURL = window.location.href;
   const development = 'http://localhost:9897';
-//   const production = 'https://nitjtt.onrender.com';
+  const production = 'https://meview.onrender.com';
 //   const nitjServer = 'https://xceed.nitj.ac.in';
   if (currentURL.includes('localhost')) {
     return development;
   }
-  // if (currentURL.includes('nitjtt.onrender.com')) {
-  //   return production;
-  // }
+  if (currentURL.includes('https://meview.onrender.com')) {
+    return production;
+  }
   // if (currentURL.includes('xceed.nitj.ac.in')) {
   //   return nitjServer;
   // }
